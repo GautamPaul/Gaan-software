c80c4e0099dcea5a4669e9bf0a15d017e7c7ec97
Gautam <paulgautam6789@gmail.com>
2024-07-04 00:20:36 +0530

    Models update, migrations file

--- src/iot/models.py
+++ src/iot/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class IOTEvent(models.Model):
-    event = models.TextField()
-    time = models.DateTimeField()
-    source = models.CharField()
-    
+    device = models.CharField(max_length=50)
+    time = models.DateTimeField(auto_now=True)
+    value = models.FloatField()
+    source = models.CharField(max_length=50)
--- src/iot/viewsets.py
+++ src/iot/viewsets.py
@@ -1,6 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import IOTEvent
+from .serializers import IOTEventSerializer
 
 
 class IOTEventsView(ModelViewSet):
     queryset = IOTEvent.objets.all()
+    serializer_class = IOTEventSerializer
