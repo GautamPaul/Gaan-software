8661ae649dae284a00681ae919a90b2eeef22168
Gautam <paulgautam6789@gmail.com>
2024-07-04 00:30:12 +0530

    Setting admin panel

--- src/iot/admin.py
+++ src/iot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import IOTEvent
 
 # Register your models here.
+
+class IOTEventAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(IOTEvent, IOTEventAdmin)
--- src/iot/models.py
+++ src/iot/models.py
@@ -6,3 +6,6 @@ class IOTEvent(models.Model):
     time = models.DateTimeField(auto_now=True)
     value = models.FloatField()
     source = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "IOT Event"          # this is used to override model name, to display in admin panel
