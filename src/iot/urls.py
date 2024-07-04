f5673d2e5a6dbf8d3d1b141dd4bec6658920bdd3
Gautam <paulgautam6789@gmail.com>
2024-07-05 00:44:07 +0530

    Summary report setup

--- src/iot/urls.py
+++ src/iot/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .viewsets import IOTEventsViewSet
+from .viewsets import IOTEventsViewSet, IOTDeviceSummary
 from .views import HomeView
 
 urlpatterns = [
     # backend
     path('', IOTEventsViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('summary/<str:device>/', IOTDeviceSummary.as_view({'get': 'retrieve'})),
 
     # frontend
     path('home/', HomeView.as_view())
--- src/iot/viewsets.py
+++ src/iot/viewsets.py
@@ -1,8 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
 from .models import IOTEvent
 from .serializers import IOTEventSerializer
 from .filters import IOTEventFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django.http import JsonResponse
 
 
 class IOTEventsViewSet(ModelViewSet):
@@ -10,3 +11,14 @@ class IOTEventsViewSet(ModelViewSet):
     serializer_class = IOTEventSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = IOTEventFilter
+
+
+class IOTDeviceSummary(ViewSet):
+    def retrieve(self, request, device):
+        from_date = request.GET.get("from_date")
+        to_date = request.GET.get("to_date")
+        print(from_date)
+        print(to_date)
+        events = IOTEvent.objects.filter(device=device)
+        print(events)
+        return JsonResponse({"test": "value"})
