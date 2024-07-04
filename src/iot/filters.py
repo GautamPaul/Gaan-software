69a37305b6b7006e327ef23e111023fd43b53d1e
Gautam <paulgautam6789@gmail.com>
2024-07-05 00:10:50 +0530

    filter import refactor

--- src/iot/filters.py
+++ src/iot/filters.py
@@ -1,9 +1,9 @@
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import FilterSet, DateFilter
 from .models import IOTEvent
 
-class IOTEventFilter(filters.FilterSet):
-    from_date = filters.DateFilter(field_name="time", lookup_expr="gte")
-    to_date = filters.DateFilter(field_name="time", lookup_expr="lte")
+class IOTEventFilter(FilterSet):
+    from_date = DateFilter(field_name="time", lookup_expr="gte")
+    to_date = DateFilter(field_name="time", lookup_expr="lte")
     class Meta:
         model = IOTEvent
         fields = ["device"]
--- src/iot/viewsets.py
+++ src/iot/viewsets.py
@@ -2,11 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 from .models import IOTEvent
 from .serializers import IOTEventSerializer
 from .filters import IOTEventFilter
-from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class IOTEventsViewSet(ModelViewSet):
     queryset = IOTEvent.objects.all()
     serializer_class = IOTEventSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = IOTEventFilter
