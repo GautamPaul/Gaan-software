1ab88618528a05c35a3739acbab1ce751dedd06a
Gautam <paulgautam6789@gmail.com>
2024-07-05 00:02:08 +0530

    added date range filter

--- src/iot/filters.py
+++ src/iot/filters.py
@@ -2,6 +2,8 @@ from django_filters import rest_framework as filters
 from .models import IOTEvent
 
 class IOTEventFilter(filters.FilterSet):
+    from_date = filters.DateFilter(field_name="time", lookup_expr="gte")
+    to_date = filters.DateFilter(field_name="time", lookup_expr="lte")
     class Meta:
         model = IOTEvent
-        fields = ["device", "time"]
+        fields = ["device"]
