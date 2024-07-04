79ce3df35e9ee6b113277125b9d0b00362354858
Gautam <paulgautam6789@gmail.com>
2024-07-04 23:49:59 +0530

    Front end setup

--- src/iot/urls.py
+++ src/iot/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .viewsets import IOTEventsViewSet
+from .views import HomeView
 
 urlpatterns = [
+    # backend
     path('', IOTEventsViewSet.as_view({'get': 'list', 'post': 'create'})),
+
+    # frontend
+    path('home/', HomeView.as_view())
 ]
--- src/iot/views.py
+++ src/iot/views.py
@@ -1,3 +1,11 @@
+import requests
+
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        response = requests.get("http://127.0.0.1:8000/iot/")
+        iot_events = response.json().get("results")
+        return render(request, "home.html", {"iot_events": iot_events})
