73089336b5421082ad30e981ef5e0903dbf1bb4d
Gautam <paulgautam6789@gmail.com>
2024-07-05 23:13:17 +0530

    updated time format in serializer

--- src/iot/serializers.py
+++ src/iot/serializers.py
@@ -1,7 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, DateTimeField
 from .models import IOTEvent
 
 class IOTEventSerializer(ModelSerializer):
+    time = DateTimeField(format="%d-%b-%Y, %H:%M:%S", read_only=True)
     class Meta:
         model = IOTEvent
         fields = "__all__"
